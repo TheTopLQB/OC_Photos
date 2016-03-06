05f0ac1cc87b2ed936a789a2fe022f28354a559d
李庆彬 <13681551812@163.com>
2016-03-06 09:40:27 +0800

    Initial Commit

---
+++ OC_Photos/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OC_Photos
+//
+//  Created by 李庆彬 on 3/6/16.
+//  Copyright © 2016 LQB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
